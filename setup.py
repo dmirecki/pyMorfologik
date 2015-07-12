2a580fa2b13f10e0c38e4357218677dcf47a11bc
Damian Mirecki <dmirecki.dm@gmail.com>
2015-07-12 19:45:40 +0200

    Jar non found fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='pyMorfologik',
     packages=['pyMorfologik'],
-    version='0.1.3',
+    version='0.1.4',
     install_requires=[],
     author='Damian Mirecki',
     author_email='dmirecki.dm@gmail.com',
