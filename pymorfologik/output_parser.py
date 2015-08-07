5505183e52836651b58570d653da5b039a7271c3
adrian <adrian.bohdanowicz@wilabs.com>
2015-08-07 12:40:03 +0200

    help added, skip_empty added

--- pymorfologik/output_parser.py
+++ pymorfologik/output_parser.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from collections import OrderedDict
 
 
 def _stem_found(stem):
@@ -17,15 +16,23 @@ _get_lines_with_stems.__annotations__ = {'morfologik_output': str,
                                          'return': list}
 
 
-def parse_for_simple_stems(output):
-    lines_with_stems = _get_lines_with_stems(output)
+def parse_for_simple_stems(input, skip_empty=False):
+    """
+    Parses the output stem lines to produce a list with possible stems
+    for each word in the input.
+
+    :param skip_empty: set True to skip lines without stems (default is False)
+    :returns: a list of tuples, each containing an original text word and
+              a list of stems for the given word
+    """
+    lines_with_stems = _get_lines_with_stems(input)
     stems = list()
 
     last_word = None
     for line in lines_with_stems:
         word, stem, _ = line.split("\t")
 
-        if not _stem_found(stem):
+        if skip_empty and not _stem_found(stem):
             continue
 
         if last_word != word:
