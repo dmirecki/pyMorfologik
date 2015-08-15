9f27e5ad8113ab194ee4e4b1e1abea1b3a3b24f1
Damian Mirecki <dmirecki.dm@gmail.com>
2015-08-15 21:11:11 +0200

    Test update after pull request

--- pymorfologik/__init__.py
+++ pymorfologik/__init__.py
@@ -1,2 +1,3 @@
 # coding=utf-8
 from .morfologik import Morfologik
+from .parsing import BaseParser, DictParser, ListParser
--- test/test_morfologik.py
+++ test/test_morfologik.py
@@ -2,20 +2,20 @@
 
 import unittest
 
-from pymorfologik import Morfologik
+from pymorfologik import Morfologik, DictParser
 
 
 class MorfologikTest(unittest.TestCase):
     def test_get_simple_stem(self):
-        stemms = Morfologik().get_simple_stem(['tańczyłem', 'gra'])
+        stemms = Morfologik().stem(['tańczyłem', 'gra'], DictParser())
         self.assertEquals(2, len(stemms))
         self.assertListEqual(['tańczyć'], stemms['tańczyłem'])
         self.assertEquals(['gra', 'grać'], stemms['gra'])
 
     def test_get_simple_stem_duplicates(self):
-        stemms = Morfologik().get_simple_stem(['gra', 'gra', 'gra'])
+        stemms = Morfologik().stem(['gra', 'gra', 'gra'], DictParser())
         self.assertEquals(1, len(stemms))
 
     def test_get_simple_stem_word_does_not_exist(self):
-        stemms = Morfologik().get_simple_stem(['niematakiegosłowa'])
+        stemms = Morfologik().stem(['niematakiegosłowa'], DictParser())
         self.assertEquals(0, len(stemms))
